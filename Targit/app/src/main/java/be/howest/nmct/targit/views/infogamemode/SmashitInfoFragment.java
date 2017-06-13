4a77434317c430ac75fccebf010134872e4275f5
Tarik <Tarik@TARIK-PC>
2017-06-13 17:32:23 +0200

    smash it info tekst

--- Targit/app/src/main/java/be/howest/nmct/targit/views/infogamemode/SmashitInfoFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/infogamemode/SmashitInfoFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import be.howest.nmct.targit.R;
 
@@ -54,6 +55,12 @@ public class SmashitInfoFragment extends Fragment {
                     mListener.playSmashit(EXTRA_DIFFICULTY_HARD);
             }
         });
+        view.findViewById(R.id.smashit_info_back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
 
         Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "font/BRLNSDB.TTF");
         Button btnSnel = (Button)view.findViewById(R.id.smashit_info_button_play_hard);
@@ -62,6 +69,24 @@ public class SmashitInfoFragment extends Fragment {
         btnMatig.setTypeface(font);
         Button btnTraag = (Button)view.findViewById(R.id.smashit_info_button_play_easy);
         btnTraag.setTypeface(font);
+
+        TextView txtDot = (TextView) view.findViewById(R.id.smashit_info_tekst_dot);
+        txtDot.setTypeface(font);
+        TextView txtDot1 = (TextView) view.findViewById(R.id.smashit_info_tekst_dot_1);
+        txtDot1.setTypeface(font);
+        TextView txtDot2 = (TextView) view.findViewById(R.id.smashit_info_tekst_dot_2);
+        txtDot2.setTypeface(font);
+        TextView txtDot3 = (TextView) view.findViewById(R.id.smashit_info_tekst_dot_3);
+        txtDot3.setTypeface(font);
+
+        TextView txtLevens = (TextView) view.findViewById(R.id.smashit_info_tekst_levens);
+        txtLevens.setTypeface(font);
+        TextView txt1 = (TextView) view.findViewById(R.id.smashit_info_tekst_1);
+        txt1.setTypeface(font);
+        TextView txt2 = (TextView) view.findViewById(R.id.smashit_info_tekst_2);
+        txt2.setTypeface(font);
+        TextView txt3 = (TextView) view.findViewById(R.id.smashit_info_tekst_3);
+        txt3.setTypeface(font);
 
         return view;
     }
