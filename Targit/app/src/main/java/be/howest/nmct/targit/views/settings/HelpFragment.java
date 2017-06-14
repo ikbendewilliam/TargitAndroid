4764c38a0fe89d32839d21d708fdea8ddee773de
Tarik <Tarik@TARIK-PC>
2017-06-14 09:12:28 +0200

    help stappen

--- Targit/app/src/main/java/be/howest/nmct/targit/views/settings/HelpFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/settings/HelpFragment.java
@@ -1,17 +1,16 @@
 package be.howest.nmct.targit.views.settings;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
+import be.howest.nmct.targit.adapters.ScreenSlidePagerAdapter;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.design.widget.TabLayout;
-import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,12 +45,15 @@ public class HelpFragment extends Fragment {
 
         //aanmaken van de fragments voor elke help stap
         stappen = new ArrayList<Fragment>();
-        stappen.add(HelpStapFragment.newInstance("Ga naar instellingen",R.drawable.help_stap_1));
-        stappen.add(HelpStapFragment.newInstance("Zet Bluetooth aan",R.drawable.help_stap_2));
+        stappen.add(HelpStapFragment.newInstance("Ga naar instellingen", R.drawable.help_stap_1));
+        stappen.add(HelpStapFragment.newInstance("Zet Bluetooth aan", R.drawable.help_stap_2));
+        stappen.add(HelpStapFragment.newInstance("Koppel de Bluetooth apparaten\n" +
+                "met de naam TARGIT", R.drawable.help_stap_3));
+        stappen.add(HelpStapFragment.newInstance("Geef de pincode 1234 in", R.drawable.help_stap_4));
 
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) view.findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager(), stappen);
         mPager.setAdapter(mPagerAdapter);
 
         // linken  van tabs met viewpager
@@ -67,26 +69,6 @@ public class HelpFragment extends Fragment {
         });
 
         return view;
-    }
-
-    /**
-     * A simple pager adapter that represents Fragment objects, in
-     * sequence.
-     */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return stappen.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return stappen.size();
-        }
     }
 
 
