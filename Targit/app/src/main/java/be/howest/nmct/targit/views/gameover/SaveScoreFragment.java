e4d3055c8d4edc3c1f59cb6d61ca330aa79efe43
ikbendewilliam <ikbendewilliam@hotmail.com>
2017-06-16 10:28:54 +0200

    nothing much

--- Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
@@ -27,23 +27,17 @@ public class SaveScoreFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         String gamemode = getArguments().getString(EXTRA_GAME);
         View view = null;
-        if(gamemode.equals(EXTRA_GAME_SMASHIT))
-                view = inflater.inflate(R.layout.fragment_save_score_smashit, container, false);
+        if (gamemode.equals(EXTRA_GAME_SMASHIT))
+            view = inflater.inflate(R.layout.fragment_save_score_smashit, container, false);
+
+
+
 
 
         return view;
-
     }
-
 }
