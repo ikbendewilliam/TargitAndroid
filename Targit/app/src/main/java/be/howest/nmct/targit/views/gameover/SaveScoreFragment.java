f496290fd1e660f0f2978662788369ac20f094ed
Downey Robersscheuten <downey95@hotmail.com>
2017-06-16 10:17:47 +0200

    prep merge 1606

--- Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
@@ -1,13 +1,10 @@
 package be.howest.nmct.targit.views.gameover;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import be.howest.nmct.targit.R;
 
@@ -39,7 +36,7 @@ public class SaveScoreFragment extends Fragment {
         String gamemode = getArguments().getString(EXTRA_GAME);
         View view = null;
         if(gamemode.equals(EXTRA_GAME_SMASHIT))
-                view = inflater.inflate(R.layout.fragment_save_score_smashit, container, false);
+                view = inflater.inflate(R.layout.fragment_save_score, container, false);
 
 
         return view;
