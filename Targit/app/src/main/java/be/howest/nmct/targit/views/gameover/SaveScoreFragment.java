586d6f3701770d728ae19b755c30fa1e1ac8188c
Downey Robersscheuten <downey95@hotmail.com>
2017-06-16 11:42:22 +0200

    prep merge 1606_2

--- Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
@@ -1,15 +1,23 @@
 package be.howest.nmct.targit.views.gameover;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import be.howest.nmct.targit.R;
 
+import static be.howest.nmct.targit.Constants.EXTRA_CATEGORY;
 import static be.howest.nmct.targit.Constants.EXTRA_GAME;
+import static be.howest.nmct.targit.Constants.EXTRA_GAME_MEMORIT;
 import static be.howest.nmct.targit.Constants.EXTRA_GAME_SMASHIT;
+import static be.howest.nmct.targit.Constants.EXTRA_GAME_ZENIT;
+import static be.howest.nmct.targit.Constants.EXTRA_SCORE;
 
 public class SaveScoreFragment extends Fragment {
 
@@ -33,10 +41,53 @@ public class SaveScoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //get arguments
         String gamemode = getArguments().getString(EXTRA_GAME);
-        View view = null;
-        if(gamemode.equals(EXTRA_GAME_SMASHIT))
-                view = inflater.inflate(R.layout.fragment_save_score, container, false);
+        String score = getArguments().getString(EXTRA_SCORE);
+        String category = getArguments().getString(EXTRA_CATEGORY);
+
+        View view = inflater.inflate(R.layout.fragment_save_score, container, false);
+
+        //set font
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "font/BRLNSDB.TTF");
+        EditText name = (EditText) view.findViewById(R.id.fragment_save_score_edittext_name);
+        name.setTypeface(font);
+        TextView txtTitle = (TextView) view.findViewById(R.id.fragment_save_score_textview_gametitle);
+        txtTitle.setTypeface(font);
+        Button btnSave = (Button) view.findViewById(R.id.fragment_save_score_button_save);
+        btnSave.setTypeface(font);
+        Button btnCancel = (Button) view.findViewById(R.id.fragment_save_score_button_cancel);
+        btnCancel.setTypeface(font);
+        TextView txtScoreTitle = (TextView) view.findViewById(R.id.fragment_save_score_textview_scoretext);
+        txtScoreTitle.setTypeface(font);
+        TextView txtScore = (TextView) view.findViewById(R.id.fragment_save_score_textview_score);
+        txtScore.setTypeface(font);
+
+        //set score
+        txtScore.setText(score);
+
+        //get the elements that need color change
+
+
+
+        //check which game mode it was
+        //TODO: insert category
+        if(gamemode.equals(EXTRA_GAME_SMASHIT)){
+            //set title name
+            txtTitle.setText("SMASH - iT");
+
+
+        }else if(gamemode.equals(EXTRA_GAME_ZENIT)){
+            //set title name
+            txtTitle.setText("ZEN - iT");
+
+
+        }else if(gamemode.equals(EXTRA_GAME_MEMORIT)){
+            //set title name
+            txtTitle.setText("MEMOR - iT");
+
+        }
+
 
 
         return view;
