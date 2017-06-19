8f625dec4f0a97308d32e48cb43bca2dbdb15f8a
Tarik <Tarik@TARIK-PC>
2017-06-19 13:31:41 +0200

    fix some text

--- Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/views/gameover/SaveScoreFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
