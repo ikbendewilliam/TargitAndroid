33aeb429f7727d1b02ff7b6fe10cfcf9eeec3f57
ikbendewilliam <ikbendewilliam@hotmail.com>
2017-06-22 10:15:50 +0200

    added final keyword

--- Targit/app/src/main/java/be/howest/nmct/targit/Constants.java
+++ Targit/app/src/main/java/be/howest/nmct/targit/Constants.java
@@ -53,7 +53,7 @@ public abstract class Constants {
     public static final String EXTRA_GAME_ZENIT = "zenit";
     public static final String EXTRA_GAME_MEMORIT = "memorit";
     public static final int STEP_TIME = 10; // The time between frames (in ms)
-    public static int COUNTDOWN_TIME = 5; //The time
+    public static final int COUNTDOWN_TIME = 5; //The time
 
     // Smashit
     public static final String EXTRA_DIFFICULTY = "difficulty";
